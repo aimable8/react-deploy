5a283e72948b8e1b2d5cb48d8b717bd500ab5955
jean <hjeanaimable@yahoo.fr>
2023-02-03 15:25:42 +0100

    code source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import image from './image.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bienvenu</h1>
+      <img src={image} alt="image" />
     </div>
   );
 }
